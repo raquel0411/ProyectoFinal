cf6c72946035d3fb600f96145cc2852061ba9d4f
raquel0411 <a01234123@tec.mx>
2023-01-31 08:45:08 -0600

    Add project files.

---
+++ ProyectoFinal/Ambiente1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class Ambiente1
+    {
+        public Ambiente1() { }
+        public object Maquina1 { get; private set; }
+        public void LlenarDatosAletoriosMaq1(int NumeroDatos, List<Variables>Maquina1,int limiteInferior, int limiteSuperior)
+        {
+            Random aleatorios = new Random(Environment.TickCount);
+            for (int i = 0; i < NumeroDatos; i++)
+            {
+                double value = (double)aleatorios.NextDouble();
+                int value4 = aleatorios.Next(limiteInferior, limiteSuperior);
+                Variables variables = new Variables();
+                variables.RateProduccion = value4;
+                Maquina1.Add(variables);
+
+            }
+        }
+        public object Demanda { get; private set; }
+        public void LLenarDatosAleatoriosDem(int NumeroDatos,List<Variables>Demanda,int limiteInferior, int limiteSuperior)
+        {
+            Random aleatorios = new Random(Environment.TickCount);
+            for (int i = 0; i < NumeroDatos; i++)
+            {
+                double value = (double)aleatorios.NextDouble();
+                double value1 = aleatorios.Next(limiteInferior, limiteSuperior);
+                double value2 = aleatorios.Next(limiteInferior, limiteSuperior);
+                int value3 = aleatorios.Next(limiteInferior, limiteSuperior);
+                Variables variables = new Variables();
+                variables.Demanda = value1;
+                variables.CantidadRequerida = value2;
+                variables.Fecha = value3;
+                Demanda.Add(variables);
+
+            }
+
+
+        }
+
+        
+            
+
+
+
+    }
+    
+}
---
+++ ProyectoFinal/variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class Variables
+    {
+        public double Demanda { get; set; }
+        public double CantidadRequerida { get; set; }
+        public int Fecha { get; set; }
+        public int RateProduccion { get; set; }
+
+        public Variables() { }
+        public Variables(Variables v1)
+        {
+            Demanda = v1.Demanda;
+            CantidadRequerida = v1.CantidadRequerida;
+            Fecha = v1.Fecha;
+            RateProduccion = v1.RateProduccion;
+
+        }
+    }
+}
